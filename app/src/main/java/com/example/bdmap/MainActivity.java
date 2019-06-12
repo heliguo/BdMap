c0394f9e08fe76bd14f270144af6b111161d9419
liguohe <ysulgh@163.com>
2019-06-12 15:04:58 +0800

    百度地图

---
+++ app/src/main/java/com/example/bdmap/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.bdmap;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LocationClient mLocationClient;
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private boolean isFirstLoc = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        mMapView = findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+        applypermission();
+        requstLocation();
+    }
+
+
+
+    private void requstLocation(){
+        initLocation();
+        mLocationClient.start();
+    }
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setScanSpan(5000);
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        mLocationClient.setLocOption(option);
+    }
+    private void navigateTo(BDLocation location){
+        if (isFirstLoc){
+            LatLng ll = new LatLng(location.getLatitude(),location.getLongitude());
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+            mBaiduMap.animateMapStatus(update);
+            update = MapStatusUpdateFactory.zoomTo(16f);
+            mBaiduMap.animateMapStatus(update);
+            isFirstLoc = false;
+        }
+        MyLocationData.Builder builder = new MyLocationData.Builder();
+        builder.latitude(location.getLatitude());
+        builder.longitude(location.getLongitude());
+        MyLocationData data = builder.build();
+        mBaiduMap.setMyLocationData(data);
+    }
+
+    public class MyLocationListener implements BDLocationListener{
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if (bdLocation.getLocType()==BDLocation.TypeGpsLocation
+                    ||bdLocation.getLocType()==BDLocation.TypeNetWorkLocation){
+                navigateTo(bdLocation);
+            }
+        }
+    }
+
+
+    //权限申请
+    String[] allpermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE};
+
+    public void applypermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            boolean needapply = false;
+            for (int i = 0; i < allpermissions.length; i++) {
+                int chechpermission = ContextCompat.checkSelfPermission(getApplicationContext(),
+                        allpermissions[i]);
+                if (chechpermission != PackageManager.PERMISSION_GRANTED) {
+                    needapply = true;
+                }
+            }
+            if (needapply) {
+                ActivityCompat.requestPermissions(this, allpermissions, 1);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stop();
+        mMapView.onDestroy();
+        mBaiduMap.setMyLocationEnabled(false);
+    }
+
+}
